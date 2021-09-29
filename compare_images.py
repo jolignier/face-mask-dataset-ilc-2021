2c207da91a1d24a579c632db4924aa68e32757ff
jolignier <jonathan0780@hotmail.fr>
2021-09-29 08:52:24 +0200

    Finished comparison script

--- compare_images.py
+++ compare_images.py
@@ -1,10 +1,10 @@
 import cv2
 import argparse
 import glob
+import numpy as np
 
-
-IMG_PATH = "./JPEGImages"
-THRESH = 0.95
+IMG_PATH = "./JPEGImages/*"
+THRESH = 65 
 
 
 def compare(img1, img2):
@@ -15,14 +15,13 @@ def compare(img1, img2):
     # Set img2 to img1 shape to compare
     height = img1.shape[0]
     width = img1.shape[1]
-    cv2.resize(img2, (width, heigth))
-    
+    img2 = cv2.resize(img2, (width, height))
     #--- take the absolute difference of the images ---
     res = cv2.absdiff(img1, img2)
     #--- convert the result to integer type ---
     res = res.astype(np.uint8)
     #--- find percentage difference based on number of pixels that are zero ---
-    percentage = (numpy.count_zero(res) * 100)/ res.size
+    percentage = 100 - ((np.count_nonzero(res) * 100)/ res.size)
     
     return percentage
     
@@ -31,12 +30,12 @@ if __name__ == '__main__':
     
     img_list = glob.glob(IMG_PATH)
     print("[INFO] - Comparison started !", len(img_list), "images to compare")
-    
     for i in range (0, len(img_list)):
         for j in range (i+1, len(img_list)):
             img1 = cv2.imread(img_list[i], 0)
             img2 = cv2.imread(img_list[j], 0)
             cmp = compare(img1, img2)
+            print(cmp)
             if cmp > THRESH:
                 print('[INFO] - Images are very close to each other, >', THRESH,'% : ', img_list[i], ' -----', img_list[2])
                 

21d3df0e6b1b4313950e5ba7c5dc535467175f8c
charbel-elhajj <charbel.elhajj@moneytrack.io>
2021-09-30 11:57:00 +0200

    initial image commit

--- rename_dir_md5.py
+++ rename_dir_md5.py
@@ -2,12 +2,25 @@ import glob
 import hashlib
 import os
 
-filenames = glob.glob("./JPEGImages/*.jpg")
-filenames += glob.glob("./JPEGImages/*.jpeg")
-filenames += glob.glob("./JPEGImages/*.JPG")
-filenames += glob.glob("./JPEGImages/*.JPEG")
+# if os.path.exists(os.path.normpath("./JPEGImages/")):
+#     print("[ERROR] Path not found: 'JPEGImages'")
+#     quit()
+
+path = "JPEGImages"
+filenames = glob.glob(os.path.join(path, "*.jpg"))
+filenames += glob.glob(os.path.join(path, "*.jpeg"))
+filenames += glob.glob(os.path.join(path, "*.JPG"))
+filenames += glob.glob(os.path.join(path, "*.JPEG"))
+
+file_old = []
+file_new = []
 
 for filename in filenames:
     with open(filename, 'rb') as inputfile:
         data = inputfile.read()
-        os.rename(filename, "./JPEGImages/" + hashlib.md5(data).hexdigest() + ".jpg")
+        file_old.append(os.path.basename(filename))
+        file_new.append(hashlib.md5(data).hexdigest() + os.path.splitext(filename)[1])
+
+        print("file_old {}".format(file_old[-1]))
+        print("file_new {}".format(file_new[-1]))
+    os.rename(os.path.join(path, file_old[-1]), os.path.join(path, file_new[-1]))
